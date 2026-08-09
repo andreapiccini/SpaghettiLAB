f3cedc595029bbd837565022d10497b47298805c
Andrea <andrea.piccini23@gmail.com>
2026-08-09 19:17:24 +0200

    port: define the port identifier

---
+++ Firmware/core/include/spaghetti/port.h
@@ -0,0 +1,15 @@
+/**
+ * @file
+ * @brief Port functionality for the Spaghetti firmware.
+ * @ingroup spaghetti_port
+ */
+
+#ifndef SPAGHETTI_PORT_H
+#define SPAGHETTI_PORT_H
+
+/**
+ * @brief Port identifier type.
+ */
+typedef uint8_t spaghetti_port_id_t;
+
+#endif /* SPAGHETTI_PORT_H */
