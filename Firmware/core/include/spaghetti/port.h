0c653fdbb7e0f44e0d5443ff663b28fa4c86977c
Andrea <andrea.piccini23@gmail.com>
2026-08-09 19:24:27 +0200

    port: define port capabilities

--- Firmware/core/include/spaghetti/port.h
+++ Firmware/core/include/spaghetti/port.h
@@ -7,9 +7,19 @@
 #ifndef SPAGHETTI_PORT_H
 #define SPAGHETTI_PORT_H
 
+#include <stdint.h>
+#include <zephyr/sys/util.h>
+
 /**
  * @brief Port identifier type.
  */
 typedef uint8_t spaghetti_port_id_t;
+
+/**
+ * @brief Enumeration of the capabilities of a Spaghetti Port.
+ */
+enum spaghetti_port_capability {
+	SPAGHETTI_PORT_CAP_I2C = BIT(0) /**< I2C capability bit. */
+};
 
 #endif /* SPAGHETTI_PORT_H */
